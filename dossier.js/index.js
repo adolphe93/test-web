719ea190da54d8eaaa65ed2a9f4027a7fb9c59aa
[adolphe93] <[dadoba.yao@uvci.edu.ci]>
2021-12-10 17:11:43 +0000

    dernier commit de la journée

---
+++ dossier.js/index.js
@@ -0,0 +1,13 @@
+const form = () => {
+    let nom = document.getElementById('nom');
+    let prenom = document.getElementById('prenom');
+    let email = document.getElementById('email');
+    let age = document.getElementById('age');
+    let homme = document.getElementById('homme');
+    let femme = document.getElementById('femme');
+    let autre = document.getElementById('autre');
+
+    let formStorage = localStorage.getItem('form')
+
+    if (formStorage == null){}
+}
